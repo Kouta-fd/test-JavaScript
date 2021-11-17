22588526af25e37a5755854eba22f06f47695e48
Kouta-fd <kotafd0124@icloud.com>
2021-11-17 18:21:53 +0900

    add:ongaku

--- slideshow/header.js
+++ slideshow/header.js
@@ -8,3 +8,7 @@ $(document).ready(function() {
         });
     });
 });
+const trigger = document.getElementsByClassName('.menu-trigger')
+trigger.addEventListener('click',() => {
+    trigger.toggleClass("onanimation");
+})
